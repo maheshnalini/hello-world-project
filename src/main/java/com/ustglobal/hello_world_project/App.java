81d803cddd7e48ce8fa6704128227b3a039647ee
mahesh <mahesh@nalini.com>
2022-06-03 19:00:53 +0530

    hello world project

---
+++ src/main/java/com/ustglobal/hello_world_project/App.java
@@ -0,0 +1,13 @@
+package com.ustglobal.hello_world_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
